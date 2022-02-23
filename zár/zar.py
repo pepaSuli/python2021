9a60d78d634b99e88597d5db54065671b3a4bbbd
pepaSuli <papp.peter@ady-nagyatad.hu>
2022-02-23 09:41:25 +0100

    4. feladat

--- zár/zar.py
+++ zár/zar.py
@@ -38,9 +38,17 @@ for index,kod in enumerate(kodok,1):
 print("A nyitó kódszámok sorai: " + " ".join(str(szam) for szam in talalat))
 
 
-
-
-
+dupla=[]
+for index,kod in enumerate(kodok,1):
+    for karakter in kod:
+        if kod.count(karakter) > 1:
+            dupla.append(index)
+            
+       
+if len(dupla)>0:
+    print(dupla[0])
+else:
+    print("Nem volt")
 
 
 
