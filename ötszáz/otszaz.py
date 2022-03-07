1d894cd7aabeab7cf34fbb966b826767eea9276b
pepaSuli <papp.peter@ady-nagyatad.hu>
2022-03-07 10:50:02 +0100

    a

--- ötszáz/otszaz.py
+++ ötszáz/otszaz.py
@@ -3,7 +3,9 @@ kosar=[]
 #szoveg=f.read()
 #print(szoveg)
 for sor in f:
-    kosar.append(sor.strip())    
+    kosar\
+           .append(sor\ 
+                   .strip())
 
 f.close()
 
@@ -13,3 +15,8 @@ print("A fizetések száma: " + str(kosar.count("F")))
 print("3. feladat")
 print("Az első vásárló " + str(kosar.index("F")) + " darab árucikket vásárolt.")
 
+print(kosar[1])
+print(kosar[1]+"aa")
+kosar[1]=kosar[1]+"aa"
+
+print(kosar[1])
