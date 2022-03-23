04a6798a134b61c0d2e21980ed69b16a916bdfb7
pepaSuli <papp.peter@ady-nagyatad.hu>
2022-03-23 09:39:43 +0100

    5. feladat eleje

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -20,5 +20,38 @@ print("3. feladat: "+str(ossz) + " fő")
 
 kulcsSzo=input("4. feladat: Kulcsszó: ")
 van=False
+
 #find()
+for e in adatok:
+    if e.find(kulcsSzo)>=0:
+        van=True
+        break
+
+if van:
+    print("Van találat!")
+else:
+    print("Nincs találat!")
+
+
+adatok2=[]
+for e in adatok:
+    temp=e.split(";")
+    temp[1]=int(temp[1])
+    temp[2]=int(temp[2])
+    adatok2.append(temp)
+print(adatok2)  
+
+talalat=[e for e in adatok if e.find(kulcsSzo)>-1]
+#print(talalat)
+if len(talalat)>0:
+    print("Van találat!")
+else:
+    print("Nincs találat!")
+
+#ferfiak-elsoosztaly;57;118
+
+
+print("5. feladat")
+
+    
 
