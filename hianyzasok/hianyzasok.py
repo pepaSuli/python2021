b1701c2a7e6cbfa13c32de21e63a8c7f8a490851
pepaSuli <papp.peter@ady-nagyatad.hu>
2022-04-01 14:04:46 +0200

    1. feladat

--- hianyzasok/hianyzasok.py
+++ hianyzasok/hianyzasok.py
@@ -10,4 +10,13 @@ honap=0
 nap=0
 for e in adatok:
     if e[0]=="#":
-            
+        # 01 15
+        honap=e[2:4]
+        nap=e[5:]
+        #print(honap)
+    else:
+        temp=[]
+        temp.append(honap)
+        temp.append(nap)
+        #név + hiányzás berakása
+  
