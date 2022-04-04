620065835709fd25b821890a61b6d224fd6da63e
Papp Péter <pepa@ADY-NAGYATAD.SULINET.HU>
2022-04-04 08:44:47 +0200

    3. feladat

--- hianyzasok/hianyzasok.py
+++ hianyzasok/hianyzasok.py
@@ -19,4 +19,26 @@ for e in adatok:
         temp.append(honap)
         temp.append(nap)
         #név + hiányzás berakása
+        vag=e.split(" ")
+        temp.append(" ".join(vag[0:2]))
+        temp.append(vag[2])
+        naplo.append(temp)
+
+print("2. feladat\nA naplóban " + str(len(naplo)) + " hiányzás van.")
+
+igazolt=0
+igazolatlan=0
+for e in naplo:
+    igazolt+=e[3].count("X")
+    igazolatlan+=e[3].count("I")
+
+
+igazolt=sum([e[3].count("X") for e in naplo])
+
+print("3. feladat\nAz igazolt hiányzások száma " + str(igazolt) + ", az igazolatlanoké " + str(igazolatlan)+" óra.")
+
+
+
+
+
   
